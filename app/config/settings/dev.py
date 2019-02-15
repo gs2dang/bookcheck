f022bc84ead8bf5f2dbdecebfe61c007f60c4cb2
gs2dang <gs2dang07@gmail.com>
2019-02-15 18:44:30 +0900

    [UPDATE] settings/base.py에서 DATABASES 분리
    settings/dev.py
    settings/prod.py

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -8,3 +8,8 @@ ALLOWED_HOSTS = []
 WSGI_APPLICATION = 'config.wsgi.dev.application'
 
 SECRETS = json.load(open(os.path.join(SECRET_DIR, 'dev.json')))
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = SECRETS['DATABASES']
--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -8,3 +8,8 @@ ALLOWED_HOSTS = []
 WSGI_APPLICATION = 'config.wsgi.prod.application'
 
 SECRETS = json.load(open(os.path.join(SECRET_DIR, 'prod.json')))
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = SECRETS['DATABASES']
