6fa5a663b8d763c0319f2f32723a34b5f531e76f
gs2dang <gs2dang07@gmail.com>
2019-02-16 18:36:37 +0900

    [UPDATE] S3Storage

--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -13,3 +13,12 @@ SECRETS = json.load(open(os.path.join(SECRET_DIR, 'prod.json')))
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = SECRETS['DATABASES']
+
+# AWS
+AWS_ACCESS_KEY_ID = SECRETS['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = SECRETS['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = SECRETS['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_REGION_NAME = 'ap-northeast-2'
+
+# S3 Storage
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
