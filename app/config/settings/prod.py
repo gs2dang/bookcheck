e91277b919d4b0709201d22b5b806b441a36d7dc
gs2dang <gs2dang07@gmail.com>
2019-02-18 20:03:48 +0900

    [UPDATE] nginx, aws 설정, Health check

--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -1,24 +1,42 @@
+import requests
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-WSGI_APPLICATION = 'config.wsgi.prod.application'
+DEBUG = False
 
 SECRETS = json.load(open(os.path.join(SECRET_DIR, 'prod.json')))
+
+ALLOWED_HOSTS = SECRETS['ALLOWED_HOSTS']
+
+WSGI_APPLICATION = 'config.wsgi.prod.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = SECRETS['DATABASES']
+# PostgreSQL
+# DATABASES = SECRETS['DATABASES']
+
+# SQLite3
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # AWS
 AWS_ACCESS_KEY_ID = SECRETS['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = SECRETS['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = SECRETS['AWS_STORAGE_BUCKET_NAME']
+AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = 'ap-northeast-2'
 
 # S3 Storage
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Health Check
+try:
+    EC2_IP = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4').text
+    ALLOWED_HOSTS.append(EC2_IP)
+except requests.exceptions.RequestException:
+    pass
